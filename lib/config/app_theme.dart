15388101e7cb15bd41e26bcc17d0114144ca885f
hjuanma <jm1995217@hotmail.com>
2023-05-17 23:19:44 -0500

    app bar style

--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -19,5 +19,6 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
         colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: true )
       );
 }
