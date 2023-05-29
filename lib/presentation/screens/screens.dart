a1e528b21916bf282a01f53b6851bb0f0aca1bc8
hjuanma <jm1995217@hotmail.com>
2023-05-29 18:46:51 -0500

    screens  created and barrel udated

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,7 @@
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial.dart';
 export 'package:widgets_app/presentation/screens/buttons/button_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/card_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
